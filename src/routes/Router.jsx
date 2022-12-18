539a6e6a97541647bb9c1ae9c91772062f033898
vitor-fontenele <vitor_fontenele@hotmail.com>
2022-12-18 01:51:26 -0300

    Attempt use Hashrouter

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter , Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import DetailsPage from "../pages/DetailsPage/DetailsPage";
 import PokedexPage from "../pages/PokedexPage/PokedexPage";
 
 const Router = () => {
     return (
-        <BrowserRouter basename="/pokeapp">
+        <HashRouter basename="/pokeapp">
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/pokedex" element={<PokedexPage />} />
                 <Route path="/details/:name" element={<DetailsPage />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
