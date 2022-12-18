2b7f82aded95f5cf3da37feb95efc103adcfa170
vitor-fontenele <vitor_fontenele@hotmail.com>
2022-12-18 02:07:30 -0300

    Pre-deploy adjust

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,17 +1,17 @@
-import { HashRouter , Routes, Route } from "react-router-dom";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import DetailsPage from "../pages/DetailsPage/DetailsPage";
 import PokedexPage from "../pages/PokedexPage/PokedexPage";
 
 const Router = () => {
     return (
-        <HashRouter basename="/pokeapp">
+        <BrowserRouter basename="/pokeapp">
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/pokedex" element={<PokedexPage />} />
                 <Route path="/details/:name" element={<DetailsPage />} />
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     )
 }
 
